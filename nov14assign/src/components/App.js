c335efb734dfc191b6d076be2d73cc6214738eb9
Suganthi2023 <suganthir2000@gmail.com>
2024-03-01 20:48:50 -0600

    Added Child Component with 3 pre-defined books

--- nov14assign/src/components/App.js
+++ nov14assign/src/components/App.js
@@ -1,10 +1,11 @@
-
+import BookList from "./BookList";
 
 function App() {
   return (
     <div className="App">
       BookList App
       <p>An Application for your Book Collection</p>
+      <BookList/>
     </div>
   );
 }
---
+++ nov14assign/src/components/BookList.js
@@ -0,0 +1,18 @@
+
+function BookList(){
+    return (
+        <div>
+        <div>
+            <li>Pride and Prejudice</li>
+        </div>,
+        <div>
+           <li>Who moved my cheese?</li>
+        </div>,
+        <div>
+            <li>The One Minute Manager</li>
+        </div>
+        </div>
+    )
+}
+
+export default BookList;
